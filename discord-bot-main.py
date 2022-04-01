622e28249dc1b73f7cf18dd7f8d8cfe6a3a76a5d
TheBeStN1ckname <88968874+TheBeStN1ckname@users.noreply.github.com>
2022-04-01 22:40:24 +0200

    Update discord-bot-main.py

--- discord-bot-main.py
+++ discord-bot-main.py
@@ -7,4 +7,4 @@ class MyClient(discord.Client):
         print('Message from {0.author}: {0.content}'.format(message))
 
 client=MyClient()
-client.run('OTU1NTcxMzk2MDE5Mzc2MTQ4.YjjnNg.wzsFShIbOTMKn--1g0mfaylcUZU')
+client.run('your-bot-token')
